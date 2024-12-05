7d70eb5ce1178aecfca9058940abf27d9772a56e
Gaby <gabriellimenesesdias@gmail.com>
2024-12-05 11:26:00 -0300

    Aula sobre variaveis

---
+++ Aula 02/Aula02.py
@@ -0,0 +1,11 @@
+print ('Good Morning')
+
+nome_usuario = 'Zezin do pó' # tipo -> str = string
+idade_usuario = 30 # tipo -> integer
+altura_usuario = 1.74 # tipo -> float = flutuante
+maior_idade = True # tipo -> bool = boolean
+
+print ('Seja bem vindo', nome_usuario)
+print ('sua idade é', idade_usuario,'anos,')
+print ('você tem', altura_usuario,'metros')
+print ('você é maior de idade?', maior_idade)
